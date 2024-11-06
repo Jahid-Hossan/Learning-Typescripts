e85bc79bc3f94e344049add513838d0722f4a3d0
Jahid-Hossan <jahidhossen02@gmail.com>
2024-11-06 12:09:46 +0300

    first commit

---
+++ 1.1-ts/index.ts
@@ -0,0 +1,14 @@
+const FirstName: String = "jahid";
+const Age: Number = 29;
+const isMarried: Boolean = false;
+let any: any;
+const notDefined: undefined = undefined;
+
+// Arrays
+
+const numbers: Number[] = [1, 2, 3, 5, 4, 58, 251];
+const arrayOfStrings: String[] = ["jahid", "hossan", "roni"];
+
+// touple
+
+const ageAndName: [Number, String] = [35, "jahid"];
